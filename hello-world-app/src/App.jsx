eda0ebd5f527619273901a9ca159de15e91418b1
sohail siddiqui <sohail@localhost>
2026-05-18 16:11:27 -0400

    feat: add Header component and integrate it into App

--- hello-world-app/src/App.jsx
+++ hello-world-app/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Header from './Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
---
+++ hello-world-app/src/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="app-header">
+      <h1>Gr4oceries List</h1>
+    </header>
+  )
+}
